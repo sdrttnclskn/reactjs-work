ead529a6454541aa8adb2c90a9697bdc0331986d
sedrettin çalışkan <sdrttnclskn@diyalektik.local>
2019-01-06 18:04:22 +0300

    ilk component

---
+++ todolist/src/todoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class TodoList extends React.Component {
+
+    render() {
+
+        return (
+
+               <div>
+                İlk Component!!!
+              </div>
+        );
+    }
+
+}
